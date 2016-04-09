77b2c77f60d52cfc2349bc29f9a1e3820acd03ae
beanstown106 <nbenis106@gmail.com>
2016-04-09 03:56:42 -0400

    Fix Android Pay SafetyNet/CTS profile issue ironically after removing the dialer jar and permission safetynet started failing with a cts profile mismatch users will pass cts after they flash my flashable google dialer zip which contains the jar/permission file add these back so users can pass safetynet without having to use the google dialer zip :)
    Revert "Drop google dialer permissions/jar"
    
    This reverts commit 85bd5a5e1fc5cc2cf80d3b3a46ac08b322d78234.
    
    Change-Id: I61447727f9d67479152454d37a82d82104a050f9

--- files/system/addon.d/70-gapps.sh
+++ files/system/addon.d/70-gapps.sh
@@ -20,6 +20,7 @@ cat <<EOF
   app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
   app/GoogleTTS/GoogleTTS.apk
   etc/permissions/com.google.android.camera.experimental2015.xml
+  etc/permissions/com.google.android.dialer.support.xml
   etc/permissions/com.google.android.maps.xml
   etc/permissions/com.google.android.media.effects.xml
   etc/permissions/com.google.android.pano.v1.xml
@@ -30,6 +31,7 @@ cat <<EOF
   etc/sysconfig/whitelist_com.android.omadm.service.xml
   etc/updatecmds/google_generic_update.txt
   framework/com.google.android.camera.experimental2015.jar
+  framework/com.google.android.dialer.support.jar
   framework/com.google.android.maps.jar
   framework/com.google.android.media.effects.jar
   framework/com.google.android.pano.v1.jar
